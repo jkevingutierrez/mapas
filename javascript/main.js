36c0a0fcaa7aef811a83588daab81325a0a5e092
Kevin Gutiérrez <kevingutierrezg@gmail.com>
2017-05-04 18:41:21 -0500

    Commit inicial

---
+++ javascript/main.js
@@ -0,0 +1,47 @@
+(function() {
+    var map;
+    var infowindow;
+
+    function initMap() {
+        var pyrmont = { lat: -33.867, lng: 151.195 };
+
+        map = new google.maps.Map(document.getElementById('googleMap'), {
+            center: pyrmont,
+            zoom: 15
+        });
+
+        var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+            location: pyrmont,
+            radius: 500,
+            type: ['store']
+        }, callback);
+    }
+
+    function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+        }
+    }
+
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        });
+    }
+
+    window.initMap = initMap;
+})();
